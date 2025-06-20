40a5d8bffa5fcde8bdf409365a797b0e00cd5fba
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-06-20 19:30:27 +0000

    Segundo commit

---
+++ prueba.js
@@ -0,0 +1,14 @@
+let num = 5;
+
+function sumar(numero){
+    let resultado = numero;
+    if(numero>5){
+        resultado = numero + 10;
+        return resultado;
+
+    }else{
+        resultado = numero * 2;
+        return resultado;
+
+    }
+}
